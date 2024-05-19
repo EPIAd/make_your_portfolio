c9a77f0bcd33a32ed36fe7083e184a7a2827d15d
ttaerrim <ltr0121@naver.com>
2024-05-19 23:04:29 +0900

    modify tax-saving-result page contents styling

--- src/components/tax-saving/result/index.tsx
+++ src/components/tax-saving/result/index.tsx
@@ -4,6 +4,31 @@ export function TaxSavingResult() {
   return (
     <section className={styles.container}>
       <h1 className={`title ${styles['title']}`}>최적 절세</h1>
+      <div className={styles['wrapper']}>
+        <div className={styles['result-wrapper']}>
+          <div className={styles.section}>
+            <p className={styles['title']}>ISA</p>
+            <div className={styles['chart']}></div>
+          </div>
+          <div className={styles.section}>
+            <p className={styles['title']}> 연금저축+IRP </p>
+            <div className={styles['chart']}></div>
+          </div>
+          <div className={styles.section}>
+            <p className={styles['title']}> 일반예금 </p>
+            <div className={styles['chart']}></div>
+          </div>
+        </div>
+        <div className={styles['result-wrapper']}>
+          <div className={`${styles.section} ${styles.contents}`}>
+            ISA 절세 효과
+          </div>
+          <div className={`${styles.section} ${styles.contents}`}>
+            연금저축+IRP 절세 효과
+          </div>
+          <div className={`${styles.section} ${styles.contents}`}>없음</div>
+        </div>
+      </div>
     </section>
   );
 }
