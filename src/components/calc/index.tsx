778e07d72226dedfbdb5ab9c867140e42f65d72e
Taerim Lee <ltr0121@naver.com>
2024-05-31 00:22:59 +0900

    modify calc page delay time

--- src/components/calc/index.tsx
+++ src/components/calc/index.tsx
@@ -10,7 +10,7 @@ export const CalcResult = ({
   useEffect(() => {
     setTimeout(() => {
       setIsShowCalc(false);
-    }, 8000);
+    }, 5000);
   }, []);
 
   if (isShowCalc) return <CalcComponent />;
