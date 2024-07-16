f4e9e6618372b95e55a883242f09047e5cf1bb0a
EPIAd <138433736+EPIAd@users.noreply.github.com>
2024-07-16 16:31:36 +0900

    Update calc.ts

--- src/components/tax-saving/result/calc.ts
+++ src/components/tax-saving/result/calc.ts
@@ -4,8 +4,12 @@ import { TaxSavingScores } from '@/shared/types/survey';
 export const calcISA = (scores: TaxSavingScores | null) => {
   if (!scores) return;
   const { s1, s2, s3 } = scores;
+
+  // s1이 200이라면 빈 문자열을 반환
+  if (s1 === 200) return '';
+
   // 금융소득종합과세 대상자일경우 ISA
-  if (s1 === 200) {
+  if (s1 === 100) {
     if (s2 === 10) return '일반형ISA';
     else if (s2 === 20 && s3 !== 1) return '일반형ISA';
     else if (s2 === 30) return '';
@@ -24,11 +28,15 @@ export const calcISAEffect = (scores: TaxSavingScores | null) => {
   if (!scores) return;
   const { s1, s2, s3 } = scores;
   const total = s1 + s2 + s3;
+
+  // s1이 200이라면 빈 문자열을 반환
+  if (s1 === 200) return '';
+
   if ((s1 === 100 && s2 === 10) || (s1 === 100 && s2 === 20 && s3 !== 1)) {
     return '최대 400만원 비과세, 이후 9.9%저율 분리과세';
   }
   if ((s1 === 200 && s2 === 10) || (s1 === 200 && s2 === 20 && s3 !== 1)) {
-    return '미정';
+    return '';
   }
   if ([221, 121].includes(total)) {
     return '없음';
@@ -47,3 +55,4 @@ export const calcB1 = (useAmount: number) => {
     return [1500, 300, useAmount - 1800];
   }
 };
+
