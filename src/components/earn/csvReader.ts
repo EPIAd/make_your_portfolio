413d8ff72eed0e4b4ad39cd2eefca75fb6e83a0a
EPIAd <138433736+EPIAd@users.noreply.github.com>
2025-05-13 10:55:04 +0900

    Update csvReader.ts

--- src/components/earn/csvReader.ts
+++ src/components/earn/csvReader.ts
@@ -14,9 +14,14 @@ export const getReturnRateDate = () => {
 };
 
 export const getReturnRate = (asset: 'ACWI' | 'EWY' | 'QQQ' | 'SPY') => {
-  return assets.map((item: ReturnRateAsset) =>
-    Number(item[asset].replace('%', '')) / 100
-  );
+  return assets.map((item: ReturnRateAsset) => {
+    // 값이 문자열이고 '%' 문자가 포함되어 있는 경우
+    if (typeof item[asset] === 'string' && item[asset].includes('%')) {
+      return Number(item[asset].replace('%', '')) / 100;
+    }
+    // 그렇지 않은 경우 (이미 소수점 형태의 퍼센트)
+    return Number(item[asset]) / 100;
+  });
 };
 
 export const getMbtiData = (mbti: string) => {
